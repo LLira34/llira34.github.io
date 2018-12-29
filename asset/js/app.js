a8e09de1315284b90152898310a79d547cfc795e
LLira34 <luislira1029@gmail.com>
2018-12-29 17:05:28 -0600

    New sidebar

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,5 +1,30 @@
-$(document).ready(function () {
-	$('#sidebarCollapse').on('click', function () {
-		$('#sidebar').toggleClass('active');
-	});
-}); 
+/**
+* Key lock of the keyboard
+*
+*/ 
+
+function keyboardKey(){
+	return false;
+}
+
+function rightClick(){
+	return false;
+}
+
+function disableText(e){
+	return false;
+}
+
+function reEnable(){
+	return true;
+}
+
+document.onselectstart = new Function("return false")
+if (window.sidebar){
+	document.onmousedown = disableText;
+	document.onclick = reEnable;
+	document.oncontextmenu = rightClick;
+	document.onkeypress = keyboardKey;
+	document.onkeydown = keyboardKey;
+	document.onkeyup = keyboardKey;
+}
